5a477f831aa473c3b8f9da4074e59c343e9f3c1c
Ian Kabaka <iankabaka1@gmail.com>
2024-10-22 21:58:18 +0300

    fixes breaking changes

--- index.js
+++ index.js
@@ -29,17 +29,15 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
-// if (require.main === module) {
-//   // add your own custom tests in here
-//   console.log("Expecting: true");
-//   console.log("=>", isPalindrome("racecar"));
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
 
-//   console.log("");
+  console.log("");
 
-//   console.log("Expecting: false");
-//   console.log("=>", isPalindrome("robot"));
-// }
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
 
-// module.exports = isPalindrome;
-console.log(isPalindrome("racecar"));
-console.log(isPalindrome("robot"));
+module.exports = isPalindrome;

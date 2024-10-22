93bbf5595b84bc4cb7036c4469c02903dd5e2076
Ian Kabaka <iankabaka1@gmail.com>
2024-10-22 21:56:46 +0300

    needs optimization

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let firstChar = word[0];
+
+  // console.log(true);
+  let wordArray = word.split("");
+  let reversedArray = wordArray.filter((char, index) => {
+    return wordArray[wordArray.length - 1 - index] === char;
+  });
+  return reversedArray.length === wordArray.length;
 }
 
 /* 
   Add your pseudocode here
+  how about before we convert the word to an array we first check if the word starts 
+  and ends with the same character  then convert it to an array? if not
+  we return false
+  will first convert the word input to an array
+  we have converted each individual character into its own element in an array.
+  in here each letter starts and ends with the same character, 
+  in here we check the characters within and compare their positions
+  get the second last character and compare it to the secodn character
+
+  
 */
 
 /*
@@ -11,15 +29,17 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+//   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
 
-module.exports = isPalindrome;
+// module.exports = isPalindrome;
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("robot"));
